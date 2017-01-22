f52d947705bd62759dd2a2922e38159dc6241082
meenushibu <meenushibu6@gmail.com>
2017-01-22 03:32:39 -0800

    mohanlal's commit

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
+
+
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+	from django.utils import timezone
+	mass=Post.objects.all()
+	return render(request, 'blog/post_list.html', {'s': mass})
